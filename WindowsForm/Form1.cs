a4fd7cd4418f9d57571da64a062ee6251edcdda2
Vitaly <valekseev@yandex.ru>
2015-09-08 00:19:52 +0300

    Init

---
+++ WindowsForm/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = (progressBar1.Value == 10 ? 0 : ++progressBar1.Value);
+            label1.Text = "Progress bar value = " + progressBar1.Value.ToString();
+        }
+    }
+}
